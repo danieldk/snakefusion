6633666c448e4642ad49697d3b46e2d5d903be60
Daniël de Kok <me@danieldk.eu>
2021-12-01 14:12:25 +0100

    Rename to snakefusion to avoid confusion

---
+++ snakefusion/__init__.py
@@ -0,0 +1,1 @@
+from .snakefusion import *
--- snakefusion/tests/conftest.py
+++ snakefusion/tests/conftest.py
@@ -1,63 +1,63 @@
 import os
 
-import finalfusion
+import snakefusion
 import pytest
 
 
 @pytest.fixture
 def analogy_fifu(tests_root):
-    yield finalfusion.Embeddings(os.path.join(tests_root, "analogy.fifu"))
+    yield snakefusion.Embeddings(os.path.join(tests_root, "analogy.fifu"))
 
 
 @pytest.fixture
 def embeddings_fifu(tests_root):
-    yield finalfusion.Embeddings(os.path.join(tests_root, "embeddings.fifu"))
+    yield snakefusion.Embeddings(os.path.join(tests_root, "embeddings.fifu"))
 
 
 @pytest.fixture
 def embeddings_text(tests_root):
-    yield finalfusion.Embeddings.read_text(os.path.join(tests_root, "embeddings.txt"))
+    yield snakefusion.Embeddings.read_text(os.path.join(tests_root, "embeddings.txt"))
 
 
 @pytest.fixture
 def embeddings_floret_check(tests_root):
-    yield finalfusion.Embeddings.read_text_dims(
+    yield snakefusion.Embeddings.read_text_dims(
         os.path.join(tests_root, "floret-check.txt")
     )
 
 
 @pytest.fixture
 def embeddings_floret_text(tests_root):
-    yield finalfusion.Embeddings.read_floret_text(
+    yield snakefusion.Embeddings.read_floret_text(
         os.path.join(tests_root, "embeddings.floret")
     )
 
 
 @pytest.fixture
 def similarity_fifu(tests_root):
-    yield finalfusion.Embeddings(os.path.join(tests_root, "similarity.fifu"))
+    yield snakefusion.Embeddings(os.path.join(tests_root, "similarity.fifu"))
 
 
 @pytest.fixture
 def similarity_pq(tests_root):
-    yield finalfusion.Embeddings(os.path.join(tests_root, "similarity-pq.fifu"))
+    yield snakefusion.Embeddings(os.path.join(tests_root, "similarity-pq.fifu"))
 
 
 @pytest.fixture
 def similarity_pq_mmap(tests_root):
-    yield finalfusion.Embeddings(
+    yield snakefusion.Embeddings(
         os.path.join(tests_root, "similarity-pq.fifu"), mmap=True
     )
 
 
 @pytest.fixture
 def subword_fifu(tests_root):
-    yield finalfusion.Embeddings(os.path.join(tests_root, "subword.fifu"))
+    yield snakefusion.Embeddings(os.path.join(tests_root, "subword.fifu"))
 
 
 @pytest.fixture
 def embeddings_text_dims(tests_root):
-    yield finalfusion.Embeddings.read_text_dims(
+    yield snakefusion.Embeddings.read_text_dims(
         os.path.join(tests_root, "embeddings.dims.txt")
     )
 
