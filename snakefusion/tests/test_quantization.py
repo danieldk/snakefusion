aec9d941964bf0504d8f315c8c1258ccf4f0acae
Daniël de Kok <me@danieldk.eu>
2021-12-02 15:18:48 +0100

    Add Embeddings.quantize
    This method can be used to quantize the embedding matrix. The `opq` and
    `gaussian_opq` quantization methods are only available if the package is
    compiled with the `accelerate` or `intel-mkl` feature.

---
+++ snakefusion/tests/test_quantization.py
@@ -0,0 +1,38 @@
+import numpy
+import pytest
+
+import snakefusion
+
+
+def test_quantization(analogy_fifu):
+    quantized = analogy_fifu.quantize(50, n_subquantizer_bits=5)
+    for embed in analogy_fifu:
+        assert numpy.allclose(quantized[embed.word], embed.embedding, atol=0.1)
+
+
+@pytest.mark.skipif(
+    snakefusion.Config.OPQ, reason="snakefusion is compiled with OPQ support"
+)
+def test_quantization_opq_fails_without_opq(analogy_fifu):
+    with pytest.raises(ValueError, match="Unsupported quantizer"):
+        analogy_fifu.quantize(50, quantizer="opq", n_subquantizer_bits=5)
+
+    with pytest.raises(ValueError, match="Unsupported quantizer"):
+        analogy_fifu.quantize(50, quantizer="gaussian_opq", n_subquantizer_bits=5)
+
+
+@pytest.mark.skipif(
+    not snakefusion.Config.OPQ, reason="snakefusion is not compiled with OPQ support"
+)
+def test_opq_quantization(analogy_fifu):
+    opq_quantized = analogy_fifu.quantize(50, quantizer="opq", n_subquantizer_bits=5)
+    for embed in analogy_fifu:
+        assert numpy.allclose(opq_quantized[embed.word], embed.embedding, atol=0.1)
+
+    gaussian_opq_quantized = analogy_fifu.quantize(
+        50, quantizer="gaussian_opq", n_subquantizer_bits=5
+    )
+    for embed in analogy_fifu:
+        assert numpy.allclose(
+            gaussian_opq_quantized[embed.word], embed.embedding, atol=0.1
+        )
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src;
+
+#[cfg(feature = "intel-mkl")]
+extern crate intel_mkl_src;
+
 use pyo3::prelude::*;
+
+mod config;
+use config::PyConfig;
 
 mod embeddings;
 use embeddings::PyEmbeddings;
@@ -8,6 +17,9 @@ use embeddings_wrap::EmbeddingsWrap;
 
 mod iter;
 use iter::{PyEmbedding, PyEmbeddingIterator};
+
+mod pq;
+use pq::PyQuantizer;
 
 mod similarity;
 use similarity::PyWordSimilarity;
@@ -24,8 +36,11 @@ use storage::PyStorage;
 /// subwords, memory-mapped matrices, and quantized matrices.
 #[pymodule]
 fn snakefusion(_py: Python, m: &PyModule) -> PyResult<()> {
+    pyo3_log::init();
+    m.add_class::<PyConfig>()?;
     m.add_class::<PyEmbeddings>()?;
     m.add_class::<PyEmbedding>()?;
+    m.add_class::<PyQuantizer>()?;
     m.add_class::<PyStorage>()?;
     m.add_class::<PyWordSimilarity>()?;
     m.add_class::<PyVocab>()?;
