ba0b35beac04ed72ad2271b478e5490192966c8f
Daniël de Kok <me@danieldk.eu>
2021-12-25 11:41:41 +0100

    Support setting a batch size for analogy/similarity queries

--- snakefusion/tests/test_similarity.py
+++ snakefusion/tests/test_similarity.py
@@ -59,31 +59,43 @@ SIMILARITY_ORDER = [
 ]
 
 
-def test_similarity_berlin_40(similarity_fifu):
-    for idx, sim in enumerate(similarity_fifu.word_similarity("Berlin", 40)):
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 6, 16, 32, None])
+def test_similarity_berlin_40(similarity_fifu, batch_size):
+    for idx, sim in enumerate(
+        similarity_fifu.word_similarity("Berlin", 40, batch_size=batch_size)
+    ):
         assert SIMILARITY_ORDER[idx] == sim.word
 
 
-def test_similarity_stuttgart_10(similarity_fifu):
-    for idx, sim in enumerate(similarity_fifu.word_similarity("Stuttgart", 10)):
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 6, 16, 32, None])
+def test_similarity_stuttgart_10(similarity_fifu, batch_size):
+    for idx, sim in enumerate(
+        similarity_fifu.word_similarity("Stuttgart", 10, batch_size=batch_size)
+    ):
         assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
 
 
-def test_embedding_similarity_stuttgart_10(similarity_fifu):
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 6, 16, 32, None])
+def test_embedding_similarity_stuttgart_10(similarity_fifu, batch_size):
     stuttgart = similarity_fifu.embedding("Stuttgart")
-    sims = similarity_fifu.embedding_similarity(stuttgart, limit=10)
+    sims = similarity_fifu.embedding_similarity(
+        stuttgart, limit=10, batch_size=batch_size
+    )
     assert sims[0].word == "Stuttgart"
 
     for idx, sim in enumerate(sims[1:]):
         assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
 
     for idx, sim in enumerate(
-        similarity_fifu.embedding_similarity(stuttgart, skip={"Stuttgart"}, limit=10)
+        similarity_fifu.embedding_similarity(
+            stuttgart, skip={"Stuttgart"}, limit=10, batch_size=batch_size
+        )
     ):
         assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
 
 
-def test_embedding_similarity_incompatible_shapes(similarity_fifu):
+@pytest.mark.parametrize("batch_size", [1, 2, 4, 6, 16, 32, None])
+def test_embedding_similarity_incompatible_shapes(similarity_fifu, batch_size):
     incompatible_embed = numpy.ones(1, dtype=numpy.float32)
     with pytest.raises(ValueError):
-        similarity_fifu.embedding_similarity(incompatible_embed)
+        similarity_fifu.embedding_similarity(incompatible_embed, batch_size=batch_size)
